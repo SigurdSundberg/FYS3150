da12cf9c28e929616276d42e70bae863c2c0c817
Sigurd Sundberg <sigurd.sundberg@gmail.com>
2020-09-24 20:48:47 +0200

    fixed functionality to be compatible with write_to_file

--- project2/code/scripts/plot_iterations_n.py
+++ project2/code/scripts/plot_iterations_n.py
@@ -6,10 +6,11 @@ import sys
 plt.rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 plt.rc('text', usetex=True)
 
-path = "../data/time_iterations_BB"
+path1 = "../data/time_iterations_ja_BB"
+path2 = "../data/time_iterations_bi_BB"
 
 
-def read_file():
+def read_file(path):
     """Spesific function for reading a spesific data file 
     """
     if not os.path.isfile(path):
@@ -37,20 +38,36 @@ def g(x, m, b):
     return m*x+b
 
 
-x, y = read_file()
+x, y = read_file(path1)
+_, y2 = read_file(path2)
 x = np.log10(x)
 y = np.log10(y)
+y2 = np.log10(y2)
 m, b = fit(x, y)
-num_values = g(x, m, b)
+m2, b2 = fit(x, y2)
 
-plt.plot(x, num_values, "k", label="$log_{10}(I)$")
+num_values = g(x, m, b)
+num_values2 = g(x, m2, b2)
+
+plt.plot(x, num_values, "k", label="Jacobi: $\log_{10}(I)$")
+plt.plot(x, num_values2, "r", label="Bisection: $\log_{10}(I)$")
+
 plt.errorbar(x, y, yerr=max_error(num_values, y),
-             fmt="o", markersize=1,  barsabove=True, label="Datapoints", capsize=3, elinewidth=2)
-plt.xlabel("$log_{10}n$")
-plt.ylabel("$log_{10}(I)$")  # Iterations
+             fmt="o", markersize=1,  barsabove=True, label="Datapoints Jacobi", capsize=3, elinewidth=2)
+plt.errorbar(x, y2, yerr=max_error(num_values2, y2),
+             fmt="ko", markersize=1,  barsabove=True, label="Datapoints Bisection", capsize=3, elinewidth=2)
+
+plt.xlabel("$\log_{10}(n)$")
+plt.ylabel("$\log_{10}(I)$")  # Iterations
 plt.title("Log/Log plot of iterations vs gridpoints")
-plt.annotate(fr"Maximum error: $\pm${max_error(x,y):.3f}", [1, 4.5])
-plt.annotate(fr"The incline is given by m = {m:.3f}", [1, 5.0])
-plt.legend()
-plt.savefig("../BB_plot/n_iter.pdf")
-# plt.show()
+
+plt.annotate(
+    fr"Maximum error Jacobi: $\pm${max_error(num_values,y):.3f}", [1, 5.5])
+plt.annotate(fr"Jacobi: m = {m:.3f}", [1, 5.25])
+plt.annotate(
+    fr"Maximum error Bisection: $\pm${max_error(num_values2,y2):.3f}", [1, 5.0])
+plt.annotate(fr"Bisection: m = {m2:.3f}", [1, 4.75])
+
+plt.legend(loc=4)
+# plt.savefig("../BB_plot/n_iter.pdf")
+plt.show()
--- project2/code/scripts/plot_timecomp.py
+++ project2/code/scripts/plot_timecomp.py
@@ -8,7 +8,7 @@ import sys
 # plt.rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 plt.rc('text', usetex=True)
 
-path = "../data/time_Comparison_BB.txt"
+path = "../data/time_Comparison_BB"
 
 
 def read_file(path):
@@ -19,7 +19,7 @@ def read_file(path):
         sys.exit(1)
     else:
         data = np.loadtxt(path)
-        return np.log10([data[:, 0], data[:, 1], data[:, 2]])
+        return np.log10([data[:, 0], data[:, 1], data[:, 2], data[:, 3]])
 
 
 def max_error(num_values, y):
@@ -43,64 +43,67 @@ def g(x, m, b):
     return m*x+b
 
 
-def get_mean(N, A, J):
+def get_mean(N, M):
     # Number of each item in the sorted list []
     k = [len(list(group)) for _, group in groupby(N)]
     n = k[0]
     c = 0
-    A_tmp = []
-    J_tmp = []
-    A_merr = []
-    J_merr = []
+    M_tmp = []
+    M_merr = []
     for _ in range(len(k)):
         for _ in range(n):
-            A_tmp.append(A[c])
-            J_tmp.append(J[c])
+            M_tmp.append(M[c])
             c += 1
-        A_merr.append(np.array(A_tmp).mean())
-        J_merr.append(np.array(J_tmp).mean())
-        A_tmp = []
-        J_tmp = []
-    return [np.array(A_merr), np.array(J_merr)]
+        M_merr.append(np.array(M_tmp).mean())
+        M_tmp = []
+    return np.array(M_merr)
 
 
-N, A, J = read_file(path)
+N, A, J, B = read_file(path)
 permute = N.argsort()
 N = N[permute]
 A = A[permute]
 J = J[permute]
+B = B[permute]
 
 # x
 key = np.array([key for key, _ in groupby(N)])
 
 # y
-A_mean = get_mean(N, A, J)[0]
-J_mean = get_mean(N, A, J)[1]
-
+A_mean = get_mean(N, A)
+J_mean = get_mean(N, J)
+B_mean = get_mean(N, B)
 
 A_m, A_b = fit(key, A_mean)
 J_m, J_b = fit(key, J_mean)
+B_m, B_b = fit(key, B_mean)
 
 key_fit = np.insert(key, 0, 0.8)
 
 A = g(key_fit, A_m, A_b)
 J = g(key_fit, J_m, J_b)
+B = g(key_fit, B_m, B_b)
 
 A_err = max_error(A, A_mean)
 J_err = max_error(J, J_mean)
+B_err = max_error(B, B_mean)
 
-plt.plot(key_fit, A, "k--", label="$log10(t)$ Armadillo")
-plt.plot(key_fit, J, "k", label="$log10(t)$ Jacobi")
+plt.plot(key_fit, A, "k--", label="$\log10(t)$ Armadillo")
+plt.plot(key_fit, J, "k", label="$\log10(t)$ Jacobi")
+plt.plot(key_fit, B, "k-.", label="$\log10(t)$ Bisection")
 
 plt.errorbar(key, J_mean, yerr=J_err,
              fmt="ro", markersize=4,  barsabove=True, label="Datapoints Jacobi", capsize=3, elinewidth=2)
 plt.errorbar(key, A_mean, yerr=A_err,
              fmt="go", markersize=4,  barsabove=True, label="Datapoints Armadillo", capsize=3, elinewidth=2)
+plt.errorbar(key, B_mean, yerr=B_err,
+             fmt="mo", markersize=4,  barsabove=True, label="Datapoints Bisection", capsize=3, elinewidth=2)
 plt.legend()
-plt.xlabel("$log_{10}n$")
-plt.ylabel("$log_{10}t$")  # Time
+plt.xlabel("$\log_{10}n$")
+plt.ylabel("$\log_{10}t$")  # Time
 plt.title("Log/Log plot of time vs gridpoints")
 plt.annotate(fr"Armadillo: m = {A_m:.2f}$\pm${A_err:.2f}", [0.75, -1])
-plt.annotate(fr"Jacobi: m = {J_m:.2f}$\pm${J_err:.2f}", [0.75, -2])
-plt.savefig("../BB_plot/timecomp.pdf")
-# plt.show()
+plt.annotate(fr"Jacobi: m = {J_m:.2f}$\pm${J_err:.2f}", [0.75, -1.33])
+plt.annotate(fr"Bisection: m = {B_m:.2f}$\pm${B_err:.2f}", [0.75, -1.66])
+# plt.savefig("../BB_plot/timecomp.pdf")
+plt.show()
