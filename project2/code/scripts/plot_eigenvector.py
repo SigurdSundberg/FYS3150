f199d75dbacd13606020f49a2bc2a2fac75f81a2
Sigurd Sundberg <sigurd.sundberg@gmail.com>
2020-09-25 18:04:33 +0200

    finished

--- project2/code/scripts/plot_eigenvector.py
+++ project2/code/scripts/plot_eigenvector.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-
-path = "../cpp_codes/output/data_BB200"
+path = "../cpp_codes/output/data_BB"
 
 # [10, 25, 75, 100, 150, 200, 300, 500]
 
 
 def read_file(path):
-    """Spesific function for reading a spesific data file 
+    """Spesific function for reading a spesific data file
     """
     if not os.path.isfile(path):
         print("File does not exists")
@@ -20,35 +19,31 @@ def read_file(path):
         return data
 
 
-def eigenvector():
-    """Creates the problem which is solved in cpp, and finds the "analytical" solutions 
-    by using numpys eigen solver for the eigenvectors. 
-    """
-    n = 200
-    h = 1/(n+1)
-    diag = 2/(h*h)
-    non_diag = -1/(h*h)
-    A = np.zeros((n, n))
-    A[0, 0] = diag
-    A[0, 1] = non_diag
-    for i in range(1, n-1):
-        A[i, i] = diag
-        A[i, i-1] = non_diag
-        A[i, i+1] = non_diag
-    A[n-1, n-1] = diag
-    A[n-1, n-2] = non_diag
-
-    values, vectors = np.linalg.eig(A)
-    permute = values.argsort()
-    vectors = vectors[:, permute]
-    v = vectors[0]
+def eigenvector(l, n):
+    v = np.zeros(n)
+    for j in range(n):
+        v[j] = np.sin(((l+1)*(j+1)*np.pi)/(n+1))
     return v
 
 
-data = read_file(path)
-values = data[:, 0]
-vectors = data[:, 1::]
+n = 200
+data = read_file(path + str(n))
+values = data[0, :]
+vectors = data[1::, :]
 permute = values.argsort()
-vector = vectors[permute, :][0]
-# plt.savefig("../BB_plot/eigenvec.pdf")
+colors = ["m", "k", "g"]
+for j in range(3):
+    vector = vectors[:, permute][:, j]
+    x = np.linspace(0, 1, n)
+    v = eigenvector(j, n)
+    relation = v[0]/vector[0]
+    plt.plot(x, v, color=colors[j], linewidth=2,
+             label=fr"Analytical: $\lambda_{j+1:d}$")
+    plt.plot(x, (vector*relation*0.9), colors[j] + "-.",
+             label=fr"Numerical: $\lambda_{j+1:d}$")
+plt.title(r"Three first eigenvectors for corresponding $\lambda_{i}$")
+plt.xlabel(r"$\rho$")
+plt.ylabel(r"$u(\rho)$")
+plt.legend()
+plt.savefig("../BB_plot/eigenvec.pdf")
 # plt.show()
